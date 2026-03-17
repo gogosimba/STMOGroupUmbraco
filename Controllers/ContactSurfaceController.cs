c78c54b55f15903eadfc066c266e48730f834687
Jonathan Johansson <111742024+gogosimba@users.noreply.github.com>
2026-03-17 18:32:59 +0100

    Final update: Added contact form, SurfaceController and validation

---
+++ Controllers/ContactSurfaceController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Website.Controllers;
+using UmbracoSetup_STMOGROUP.Models;
+
+namespace UmbracoSetup_STMOGROUP.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public ContactSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider)
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        [HttpPost]
+        public IActionResult HandleSubmit(ContactFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            // Här skulle man normalt skicka mejl eller spara i databas
+            TempData["Success"] = true;
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ Models/ContactFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoSetup_STMOGROUP.Models
+{
+    public class ContactFormViewModel
+    {
+        [Required(ErrorMessage = "Vänligen fyll i ditt namn")]
+        public string Name { get; set; } = default!;
+
+        [Required(ErrorMessage = "Vänligen fyll i din e-post")]
+        [EmailAddress(ErrorMessage = "Vänligen ange en giltig e-postadress")]
+        public string Email { get; set; } = default!;
+
+        [Required(ErrorMessage = "Meddelandet får inte vara tomt")]
+        [MinLength(10, ErrorMessage = "Meddelandet måste vara minst 10 tecken")]
+        public string Message { get; set; } = default!;
+    }
+}
